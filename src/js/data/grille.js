56d1267f4ae18cec30fbad938612ecb30bdcd4bf
DI FABIO Lorenzo <difabiolorenzo@live.fr>
2023-12-07 23:17:44 +0100

    0.0.2
    - Correction affichage réponse
    - Ajout affichage en grille du numéro du mot

--- src/js/data/grille.js
+++ src/js/data/grille.js
@@ -1,15 +1,15 @@
 game.grilles = TAFFY([
     {"grille_id":1,"grille":[
-            {"id":1,"mot":"DELICATE","x":1,"y":0,"orientation":"v","definition":"Comme une douce attention"},
-            {"id":2,"mot":"YAOURT","x":3,"y":0,"orientation":"v","definition":"Il a du pot"},
-            {"id":3,"mot":"MOITE","x":4,"y":3,"orientation":"v","definition":"Chaud et humide"},
-            {"id":4,"mot":"BOHEME","x":5,"y":0,"orientation":"v","definition":"Comme une vie vagabonde"},
-            {"id":5,"mot":"MUSARDER","x":7,"y":0,"orientation":"v","definition":"Aller le nez au vent"},
-            {"id":6,"mot":"DEMAGOGUE","x":0,"y":1,"orientation":"h","definition":"Il nous dit ce qu'on veut entrendre"},
-            {"id":7,"mot":"BITUME","x":0,"y":3,"orientation":"h","definition":"Il s'étale sur la route"},
-            {"id":8,"mot":"ROMPRE","x":3,"y":4,"orientation":"h","definition":"Verbe à tout casser"},
-            {"id":9,"mot":"PARTIE","x":0,"y":5,"orientation":"h","definition":"La deuxième à un goût de revenche"},
-            {"id":10,"mot":"REFLETER","x":0,"y":7,"orientation":"h","definition":"Renvoyer une bonne image"}
+            {"id":1,"mot":"DELICATE","y":0,"x":1,"orientation":"v","definition":"Comme une douce attention"},
+            {"id":2,"mot":"YAOURT","y":0,"x":3,"orientation":"v","definition":"Il a du pot"},
+            {"id":3,"mot":"MOITE","y":3,"x":4,"orientation":"v","definition":"Chaud et humide"},
+            {"id":4,"mot":"BOHEME","y":0,"x":5,"orientation":"v","definition":"Comme une vie vagabonde"},
+            {"id":5,"mot":"MUSARDER","y":0,"x":7,"orientation":"v","definition":"Aller le nez au vent"},
+            {"id":6,"mot":"DEMAGOGUE","y":1,"x":0,"orientation":"h","definition":"Il nous dit ce qu'on veut entrendre"},
+            {"id":7,"mot":"BITUME","y":3,"x":0,"orientation":"h","definition":"Il s'étale sur la route"},
+            {"id":8,"mot":"ROMPRE","y":4,"x":3,"orientation":"h","definition":"Verbe à tout casser"},
+            {"id":9,"mot":"PARTIE","y":5,"x":0,"orientation":"h","definition":"La deuxième à un goût de revenche"},
+            {"id":10,"mot":"REFLETER","y":7,"x":0,"orientation":"h","definition":"Renvoyer une bonne image"}
         ]
     }
 ])
--- src/js/main.js
+++ src/js/main.js
@@ -16,17 +16,11 @@ function initGame() {
 
 function createGrid(li, col) {
     var table_html = "<table>"
-    for (var i=0; i < (li+1); i++) {
+    for (var i=0; i < (li); i++) {
         table_html += "<tr>"
-        for (var j=0; j < (col+1); j++) {
-            if (i==0 || j==0) {
-                if (i==0) {}
-                var HTMLclass="number_cell"
-            } else {
-                var HTMLclass="empty_cell"
-            }
+        for (var j=0; j < (col); j++) {
+            var HTMLclass="empty_cell"
             table_html += `<td id="cell_${i}_${j}" class="${HTMLclass}"></td>`
-            
         }
         table_html += "</tr>"
     }
@@ -41,15 +35,27 @@ function fillGrid() {
     for (var i=0; i<game.grille_en_cours.length; i++) {
         var word_data = game.grille_en_cours[i]
         var word = word_data.mot
-        var x = word_data.x + 1
-        var y = word_data.y + 1
+        var x = word_data.x
+        var y = word_data.y
         var orientation = word_data.orientation
+        var id = word_data.id
+
+        
+        if (orientation == "h") {
+            var number_cell = document.getElementById(`cell_${y+1}_${x}`)
+            number_cell.innerHTML = id
+            number_cell.className = "number_cell"
+        } else {
+            var number_cell = document.getElementById(`cell_${y}_${x+1}`)
+            number_cell.innerHTML = id
+            number_cell.className = "number_cell"
+        }
 
         for (var j=0; j<word.length; j++) {
             if (orientation == "h") {
-                var cell = document.getElementById(`cell_${x+j}_${y}`)
+                var cell = document.getElementById(`cell_${y+1}_${x+1+j}`)
             } else {
-                var cell = document.getElementById(`cell_${x}_${y+j}`)
+                var cell = document.getElementById(`cell_${y+1+j}_${x+1}`)
             }
 
             // AFFICHAGE HASARD LETTRE: A CHANGER
@@ -69,21 +75,19 @@ function getRandomDefinition() {
     var r = Math.floor(Math.random() * question_lettre.length);
     var data = game.question_lettre().get()[r]
     var lettre = data.reponse;
-    definition.innerHTML = data.definition;
+    definition_question.innerHTML = data.definition;
     definition_lettre.innerHTML = lettre
     definition_explication.innerHTML = replaceArobaseByLetter(data.explication);
 
     function replaceArobaseByLetter(text) {
         var replacedText = text
+        var replacementText = "<span id=\"definition_highlight\">"+ lettre +"</span>"
         for (var i=0; i<text.length; i++) {
+            console.log(replacedText)
             replacedText.indexOf("@")
-            if (i != -1) {
-
-            } else {
-                break;
-            }
+            console.log(replacedText.indexOf("@"))
         }
-        return replaceAt(text, text.indexOf("@"), "<span id=\"definition_highlight\">"+ lettre +"</span>", 0);
+        return replaceAt(text, text.indexOf("@"), replacementText, 0);
     }
     
     function eraseAfterArobase(text) {
